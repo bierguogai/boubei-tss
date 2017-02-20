397df12af3be095a3059fdfe2f989e6fbd449dd7
Jon.King <jinpujun@gmail.com>
2017-02-20 19:07:43 +0800

    fix

--- syncTssAll.sh
+++ syncTssAll.sh
@@ -1,6 +1,10 @@
 
 cp -r ../boubei-tss/src/main/webapp/  src/main/webapp/
 
+# cp -r ../boubei-tss/src/main/java/com/boubei/tss/util  src/main/java/com/boubei/tss/S
+# cp -r ../boubei-tss/src/main/java/com/boubei/tss/cache  src/main/java/com/boubei/tss/
 cp -r ../boubei-tss/src/main/java/com/boubei/tss/dm  src/main/java/com/boubei/tss/
+
+
 cp -r ../boubei-tss/target/tss-all.jar  tools/
 mv tools/tss-all.jar  tools/tss-4.1.jar
